c019fd5c27dd450309758746e8a5e6b789918ae1
chjcago2001 <88874679+chjcago2001@users.noreply.github.com>
2022-12-10 16:10:22 +0700

    up

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -78,24 +78,32 @@ const Dashboard: NextPage<Props> = ({ isOpenDashboard, setOpen, orders }) => {
             open={isOpenDashboard}
             onClose={handleClose}
         >
-            <DialogTitle textAlign={"center"}>Dashboard</DialogTitle>
+            <DialogTitle textAlign={"center"}>Thống kê doanh thu</DialogTitle>
             <Divider />
-            <DialogContent className="lg:w-[60rem] w-full h-full">
-                <TableContainer component={Paper}>
+            <DialogContent className="lg:w-[70vw] w-full h-full">
+                <TableContainer
+                    className="!overflow-x-hidden"
+                    component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
                             <TableRow>
                                 <TableCell>STT</TableCell>
-                                <TableCell align="right">Loại đơn hàng</TableCell>
-                                <TableCell align="right">Người đặt hàng</TableCell>
-                                <TableCell align="right">Chủ sản phẩm</TableCell>
-                                <TableCell align="center">Trạng thái</TableCell>
-                                <TableCell align="right">Tổng tiền</TableCell>
-                                <TableCell align="right">Phí dịch vụ (10%)</TableCell>
-                                <TableCell align="right">Ngày đặt hàng</TableCell>
+                                <TableCell align="right" className="whitespace-nowrap">Loại đơn hàng</TableCell>
+
+                                <TableCell align="right" className="whitespace-nowrap">Người đặt hàng</TableCell>
+
+                                <TableCell align="right" className="whitespace-nowrap">Chủ sản phẩm</TableCell>
+                                <TableCell align="center" className="whitespace-nowrap">Trạng thái</TableCell>
+
+                                <TableCell align="right" className="whitespace-nowrap">Tổng tiền</TableCell>
+
+                                <TableCell align="right" className="whitespace-nowrap">Phí dịch vụ (10%)</TableCell>
+
+                                <TableCell align="right" className="whitespace-nowrap">Ngày đặt hàng</TableCell>
                             </TableRow>
                         </TableHead>
-                        <TableBody>
+                        <TableBody
+                        >
                             {data.map((item: any, index) => (
                                 <TableRow
                                     onClick={() => handleOpenOrderDetail(item.id)}
@@ -109,18 +117,18 @@ const Dashboard: NextPage<Props> = ({ isOpenDashboard, setOpen, orders }) => {
                                     <TableCell align="right">{item.type === "booking" ? "Sân bóng đá" : "Sản phẩm"}</TableCell>
                                     <TableCell align="right">{item.orderer}</TableCell>
                                     <TableCell align="right">{item.owner}</TableCell>
-                                    {item.type === "booking" && item.status === 0 && <TableCell className="text-blue-500" align="right">{"Chưa thanh toán"}</TableCell>}
-                                    {item.type === "booking" && item.status === 3 && <TableCell className="text-primary" align="right">{"Xác nhận đặt sân thành công"}</TableCell>}
-                                    {item.type === "booking" && item.status === 4 && <TableCell className="text-red-500" align="right">{"Từ chối đặt sân"}</TableCell>}
-                                    {item.status === 2 && <TableCell className="text-yellow-500" align="right">{"Chờ xác nhận"}</TableCell>}
+                                    {item.type === "booking" && item.status === 0 && <TableCell className="text-blue-500" align="center">{"Chưa thanh toán"}</TableCell>}
+                                    {item.type === "booking" && item.status === 3 && <TableCell className="text-primary whitespace-nowrap" align="center">{"Xác nhận đặt sân thành công"}</TableCell>}
+                                    {item.type === "booking" && item.status === 4 && <TableCell className="text-red-500" align="center">{"Từ chối đặt sân"}</TableCell>}
+                                    {item.status === 2 && <TableCell className="text-yellow-500" align="center">{"Chờ xác nhận"}</TableCell>}
 
-                                    {item.type === "order" && item.status === 3 && <TableCell className="text-primary" align="right">{"Xác nhận đơn hàng thành công"}</TableCell>}
-                                    {item.type === "order" && item.status === 4 && <TableCell className="text-red-500" align="right">{"Từ chối đơn hàng"}</TableCell>}
-                                    {item.type === "order" && item.status === 5 && <TableCell className="text-yellow-500" align="right">{"Chờ lấy hàng"}</TableCell>}
-                                    {item.type === "order" && item.status === 6 && <TableCell className="text-yellow-500" align="right">{"Đang giao"}</TableCell>}
-                                    {item.type === "order" && item.status === 7 && <TableCell className="text-primary" align="right">{"Giao hàng thành công"}</TableCell>}
-                                    {item.type === "order" && item.status === 8 && <TableCell className="text-red-500" align="right">{"Từ chối nhận hàng"}</TableCell>}
-                                    {item.type === "order" && item.status === 9 && <TableCell className="text-primary" align="right">{"Đã hoàn tiền đặt hàng"}</TableCell>}
+                                    {item.type === "order" && item.status === 3 && <TableCell className="text-primary whitespace-nowrap" align="center">{"Xác nhận đơn hàng thành công"}</TableCell>}
+                                    {item.type === "order" && item.status === 4 && <TableCell className="text-red-500" align="center">{"Từ chối đơn hàng"}</TableCell>}
+                                    {item.type === "order" && item.status === 5 && <TableCell className="text-yellow-500" align="center">{"Chờ lấy hàng"}</TableCell>}
+                                    {item.type === "order" && item.status === 6 && <TableCell className="text-yellow-500" align="center">{"Đang giao"}</TableCell>}
+                                    {item.type === "order" && item.status === 7 && <TableCell className="text-primary" align="center">{"Giao hàng thành công"}</TableCell>}
+                                    {item.type === "order" && item.status === 8 && <TableCell className="text-red-500" align="center">{"Từ chối nhận hàng"}</TableCell>}
+                                    {item.type === "order" && item.status === 9 && <TableCell className="text-primary" align="center">{"Đã hoàn tiền đặt hàng"}</TableCell>}
 
                                     <TableCell align="right">{item.total}</TableCell>
                                     <TableCell align="right">{item.totalServiceFee}</TableCell>
@@ -144,10 +152,11 @@ const Dashboard: NextPage<Props> = ({ isOpenDashboard, setOpen, orders }) => {
                         ) - +orders.filter((order: Order) => order.status === 3 || order.status === 7).reduce(
                             (previousValue, currentValue) => previousValue + currentValue.total,
                             0
-                        ) / 100 * +process.env.NEXT_PUBLIC_SERVICE_FEE! : +orders.filter((order: Order) => order.status === 3 || order.status === 7).reduce(
-                            (previousValue, currentValue) => previousValue + currentValue.total,
-                            0
-                        ) / 100 * +process.env.NEXT_PUBLIC_SERVICE_FEE!} currency="VND" pattern="##,### !" />
+                        ) / 100 * +process.env.NEXT_PUBLIC_SERVICE_FEE!
+                            : +orders.filter((order: Order) => order.status === 3 || order.status === 7).reduce(
+                                (previousValue, currentValue) => previousValue + currentValue.total,
+                                0
+                            ) / 100 * +process.env.NEXT_PUBLIC_SERVICE_FEE!} currency="VND" pattern="##,### !" />
                     </Typography>
                 </DialogContentText>
             </DialogActions>
