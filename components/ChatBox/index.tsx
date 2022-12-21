9e40256415e7df8d208264bffba178ade9bb2d2e
chjcago2001 <88874679+chjcago2001@users.noreply.github.com>
2022-12-22 00:00:20 +0700

    up

--- components/ChatBox/index.tsx
+++ components/ChatBox/index.tsx
@@ -156,13 +156,13 @@ const ChatBox = () => {
     }, [])
 
 
-    useLayoutEffect(() => {
-        if (user?.role === "admin") {
-            const listUsersOnline: User[] | any = usersOnline.filter((u: SocketUser) => u.userId !== user?.id).map((u: SocketUser) => conversationsRef?.current?.find((c: User) => c?.id === u?.userId))
-            const listUsersOffline = usersRef?.current?.filter((c: User) => listUsersOnline?.some((u: User) => u?.id !== c?.id && c.id !== user?.id))
-            conversationsRef.current = [...listUsersOnline, ...listUsersOffline]
-        }
-    }, [usersOnline.length])
+    // useLayoutEffect(() => {
+    //     if (user?.role === "admin") {
+    //         const listUsersOnline: User[] | any = usersOnline.filter((u: SocketUser) => u.userId !== user?.id).map((u: SocketUser) => conversationsRef?.current?.find((c: User) => c?.id === u?.userId))
+    //         const listUsersOffline = usersRef?.current?.filter((c: User) => listUsersOnline?.some((u: User) => u?.id !== c?.id && c.id !== user?.id))
+    //         conversationsRef.current = [...listUsersOnline, ...listUsersOffline]
+    //     }
+    // }, [usersOnline.length])
 
 
     const handleSendMessage = useCallback(async (e: any) => {
